4c79b0fb1de49bbbca4bd6696276447bbaaede35
Abdullah Mohammad Khan <31942235+AbdullahMohammadKhan@users.noreply.github.com>
2017-10-19 13:05:08 +0600

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,10 @@
+<div class="col-md-3">
+    <div class="list-group">
+  <a href="index.php" class="list-group-item active navbar-inverse"> Home</a>
+  <a href="profile02.php" class="list-group-item">Profile</a>
+  <a href="sub_form.php" class="list-group-item">subject List</a>
+  <a href="edit01.php" class="list-group-item">Edit Roll</a>
+  <a href="edit02.php" class="list-group-item">Edit Info</a>
+  <a href="edit_choice.php" class="list-group-item">Edit Subject</a>
+</div>
+</div>
